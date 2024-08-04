b057c2c08855e4f0c8e330818a9e7a523cf5b211
Luigi Perillo <Luigi Perillo@DESKTOP-HBB0CPB>
2024-08-03 22:04:24 -0300

    feat: add acc model

---
+++ DonderfulUtils/Model/AccData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DonderfulUtils.Model
+{
+    public class AccData
+    {
+        public List<AccItens> items { get; set; }
+    }
+
+    public class AccItens
+    {
+        public int number { get; set; }
+        public string Id { get; set; }
+        public bool userNotSelectable { get; set; }
+        public bool flipTexture { get; set; }
+        public string thumbnailPath { get; set; }
+        public string nameKey { get; set; }
+        public string descriptionKey { get; set; }
+        public string DLC { get; set; }
+
+    }
+}
